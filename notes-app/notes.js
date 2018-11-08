e975c9d6c26965ec036bbc46fd4233a1473d2a94
faiza.noor <faizanoor3001@gmail.com>
2018-11-09 03:19:06 +0530

    making the projects alighned in specific folders
    Created new folder for weather-app and moved notes app files in a folder named notes-app

---
+++ notes-app/notes.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch(e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json' , JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+var getAll = () => {
+  var notes = fetchNotes();
+  if(notes) {
+    return notes;
+  }
+};
+
+var getNote = (title) => {
+  var notes = fetchNotes();
+  var specificNote = notes.filter((note) => note.title === title);
+  return specificNote[0];
+};
+
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+  return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+  console.log('Note is -------');
+  console.log(`Title -- ${note.title}`);
+  console.log(`Body -- ${note.body}`);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
+};
