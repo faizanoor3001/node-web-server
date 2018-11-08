8aae77c7a83bc65965e9e0067d9876337ecf09f5
faiza.noor <faizanoor3001@gmail.com>
2018-11-09 03:10:14 +0530

    Adding notes app project files
    Adding notes app project files - app.js and notes.js

--- app.js
+++ app.js
@@ -1,42 +1,75 @@
-console.log('Starting app.');
-
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
+var titleOptions = {
+                  describe:'Title of note',
+                  demand: true,
+                  alias: 't'
+                };
+var bodyOptions = {
+                  describe:'Body of note',
+                  demand: true,
+                  alias: 'b'
+                };
+const argv = yargs
+                .command('add' , 'Add a new note', {
+                    title: titleOptions,
+                    body: bodyOptions
+                })
+                .command('read' , 'Read a new note', {
+                    title: titleOptions
+                })
+                .command('list' , 'Get all the note(s) in the file')
+                .command('remove' , 'Remove a note', {
+                    title: titleOptions
+                })
+                .help()
+                .argv;
+//console.log(argv);
+// var command = process.argv[2];
+var command = argv._[0];
+//console.log('Command' , command);
+//console.log(process.argv);
+if (command === 'add') {
+  var note = notes.addNote(argv.title, argv.body);
+  //if(!(typeof note === 'undefined')) {
+  // code only gonna run if note is an object else it will be undefined hence it will be false
+  if(note) {
+    notes.logNote(note);
+  } else {
+    console.log('Title '+ argv.title +' already exists !! Please try with another title');
+  }
+} else if (command === 'list') {
+  var notesList = notes.getAll();
+  console.log(`Printing ${notesList.length} note(s)`);
+  debugger;
+  if(notesList) {
+  //_.each((notesList), function(body, title ) { console.log(title , +' ' + body );});
+  // -- correct lodash foreach function
+  _.forEach(notesList, function(note) {
+    //console.log(note);
+    notes.logNote(note);});
+  // --correct array forEach function
+  //notesList.forEach(function(note) {notes.logNote(note)});
+  // wrong -->>_.each((notesList), function(value, key ) { console.log(key);});
+}
+} else if (command === 'remove') {
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note was not found';
+  console.log(message);
 
-// var res = notes.addNote();
-// console.log(res);
-//
-// var addRes = notes.add(9,-1);
-// console.log(addRes);
+}else if (command === 'read') {
+  var specificNote = notes.getNote(argv.title);
 
-// to include a 3rd party module
-var _ = require('lodash');
-console.log(_.isString(123));
-
-
-var arr = [1,2,3,4,5,5,4]
-console.log(_.uniq(arr))
-
-_.times(5,()=> console.log('foo'));
-
-
-// var user = os.userInfo();
-//console.log(user);
-
-// option one
-// fs.appendFile('greetings.txt', 'Hello World! To' + user.username + '!', function(err) {
-//   if(err) {
-//     console.log('Unable to write to file');
-//   }
-// });
-
-// template strings
-// fs.appendFile('templateString.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
-//   if(err) {
-//     console.log('Unable to write to file');
-//   }
-// });
-
-// option two
-// fs.appendFileSync('greetMe.txt', 'Hi There !' + user.username + '! You are in the directory' + user.homedir + '.');
+  if(specificNote) {
+      notes.logNote(specificNote);
+  }
+  else {
+    console.log('Note not found');
+  }
+  }
+  else {
+  console.log('Command not recognised');
+}
--- notes.js
+++ notes.js
@@ -1,14 +1,77 @@
-console.log("Starting notes.js")
-// console.log(module)
+const fs = require('fs');
 
-module.exports.age = 27
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch(e) {
+    return [];
+  }
+};
 
-module.exports.addNote = () => {
-  console.log("inside addNote")
-  return "NewNote"
-}
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json' , JSON.stringify(notes));
+};
 
-module.exports.add = (a,b) => {
-  console.log("inside the addition function")
-  return (a+b)
-}
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+  //
+  // var duplicateNotes = notes.filter((note) => {
+  //   return note.title === title;
+  // });
+
+   //simpler above code
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+  //fs.appendFileSync('notes-data.json' , noteText);
+};
+
+var getAll = () => {
+  var notes = fetchNotes();
+  if(notes) {
+    return notes;
+  }
+};
+
+var getNote = (title) => {
+  var notes = fetchNotes();
+  var specificNote = notes.filter((note) => note.title === title);
+  // console.log(specificNote); // this returns an array , to get the specific value of title and body ,we pass as specificNote[0]
+  return specificNote[0];
+};
+
+var removeNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+  return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+  console.log('Note is -------');
+  console.log(`Title -- ${note.title}`);
+  console.log(`Body -- ${note.body}`);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
+};
+
+// module.exports.addNote = () => {
+//   console.log("inside addNote")
+//   return "NewNote"
+// }
