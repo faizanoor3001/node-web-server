b64c89ba70d62a72f2fb1ba25b323878edee77e6
faiza.noor <faizanoor3001@gmail.com>
2018-11-24 07:30:26 +0530

    Rendering Templates with hbs
    Added two files about.hbs and home.hbs

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,23 +1,41 @@
 const express = require('express');
+const hbs = require('hbs');
 var app = express();
 
+
 //middleware function
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+//templating
 
 // root route and another argument is function
 app.get('/', (req, res) => {
   //res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'Faiza',
-    likes: [
-      'Procastinating',
-      'Sleeping'
-    ]
+  // res.send({
+  //   name: 'Faiza',
+  //   likes: [
+  //     'Procastinating',
+  //     'Sleeping'
+  //   ]
+  // })
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'welcome to Node js Rendering Template lesson.',
+    authorName: 'Faiza Noor'
   })
 });
 
 app.get('/about', (req,res) => {
-  res.send('<h1>About Page</h1>');
+  //res.send('<h1>About Page</h1>');
+  // to pass data by adding s2nd argument
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(),
+    aboutMessage: 'It is about the express server.',
+    authorName: 'Faiza Noor'
+  });
 });
 
 app.get('/bad', (req, res) =>{
