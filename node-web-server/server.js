c23343dc32240150ad271032f00370ffe72ba89c
faiza.noor <faizanoor3001@gmail.com>
2018-11-24 09:00:37 +0530

    express middleware options
    generated log files and maintainence page

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 
 
 //partial files
 hbs.registerPartials(__dirname + '/views/partials');
-//middleware function
+
+//express middleware function
 app.set('view engine', 'hbs');
+
+
+app.use( (req, res, next)=> {
+  var now = new Date().toString();
+  var log = `${now} ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err)=> {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  })
+  next();
+});
+
+// it will leave all the other handlers or routes from executing
+app.use(( req, res, next)=>{
+  res.render('maintainence.hbs', {
+      underMaintainence: 'We will be right back! Under maintainence'
+  })
+});
+
 app.use(express.static(__dirname + '/public'));
 
 //helper eg: to compute data which is required by every page
